02d34896b2d97f6011b7fb464838b45aabec0d70
jcrowe91 <jcrowe9119@gmail.com>
2022-06-01 20:57:55 -0500

    Initial commit

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ console.log("Hello World!\n==========\n");
 console.log("Follow the steps in the README.md file to complete the exercises:\n==========\n");
 
 // Exercise 1
+let favNum = 9;
+let userNum = window.prompt("What number do you guess?");
+if (userNum > favNum) {
+    console.log("Too high!");
+}
+else if (userNum < favNum){
+    console.log("Too low!");
+}
+else{
+    console.log("Congrats!")
+}
 
 
 // Exercise 2
